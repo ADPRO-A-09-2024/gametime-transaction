731fe0108abc5a94bcb4fd1a91a45651b7609937
RobertBenyamin <robertbenyamino@gmail.com>
2024-04-06 16:30:31 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Game Time
+
+## Anggota Kelompok
+1. Rachel Heningtyas Zanetta Erari - 2206081944
+2. Henry Soedibjo - 2206827762
+3. Kevin Ignatius Wijaya - 2206083470
+4. Robert Benyamin - 2206817383
+5. Nadya Hoesin - 2106651673
---
+++ src/test/java/id/ac/ui/cs/advprog/gametime/transaction/TransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.gametime.transaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
