21b4707984b546e573eedace8956eb3dab438c36
RobertBenyamin <robertbenyamino@gmail.com>
2024-04-17 11:15:45 +0700

    Add Continuous Deployment workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,9 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	runtimeOnly("org.postgresql:postgresql")
 	compileOnly("org.projectlombok:lombok")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
