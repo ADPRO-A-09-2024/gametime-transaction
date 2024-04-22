54bcf0c83a519129fc2d26a3c8012c6d8ae9c39e
RobertBenyamin <robertbenyamino@gmail.com>
2024-04-22 20:35:36 +0700

    feat: create user model

---
+++ src/main/java/id/ac/ui/cs/advprog/gametime/transaction/model/Builder/UserBuilder.java
@@ -0,0 +1,80 @@
+package id.ac.ui.cs.advprog.gametime.transaction.model.Builder;
+
+import id.ac.ui.cs.advprog.gametime.transaction.model.Enum.UserRole;
+import id.ac.ui.cs.advprog.gametime.transaction.model.User;
+
+import java.lang.IllegalArgumentException;
+
+public class UserBuilder {
+    private String username;
+    private String email;
+    private String password;
+    private String role;
+
+    private int balance;
+
+    public UserBuilder username(String username) {
+        if (username == null || username.isEmpty()) {
+            throw new IllegalArgumentException("Username cannot be empty");
+        }
+        if (username.length() < 4) {
+            throw new IllegalArgumentException("Username must be at least 4 characters long");
+        }
+        if (!username.matches("^[a-zA-Z0-9_]*$")) {
+            throw new IllegalArgumentException("Username can only contain letters, numbers, and underscore");
+        }
+        this.username = username;
+        return this;
+    }
+
+    public UserBuilder email(String email) {
+        if (email == null || email.isEmpty()) {
+            throw new IllegalArgumentException("Email cannot be empty");
+        }
+        if (!email.matches("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$")) {
+            throw new IllegalArgumentException("Invalid email format");
+        }
+        this.email = email;
+        return this;
+    }
+
+    public UserBuilder password(String password) {
+        if (password == null || password.isEmpty()) {
+            throw new IllegalArgumentException("Password cannot be empty");
+        }
+        if (password.length() < 8) {
+            throw new IllegalArgumentException("Password must be at least 8 characters long");
+        }
+        this.password = password;
+        return this;
+    }
+
+    public UserBuilder role(String role) {
+        if (role == null || role.isEmpty()) {
+            throw new IllegalArgumentException("Role cannot be empty");
+        }
+        if (!UserRole.contains(role)) {
+            throw new IllegalArgumentException("Invalid role");
+        }
+        this.role = role;
+        return this;
+    }
+
+    public UserBuilder balance(int balance) {
+        if (balance < 0) {
+            throw new IllegalArgumentException("Balance cannot be negative");
+        }
+        this.balance = balance;
+        return this;
+    }
+
+    public User build() {
+        User user = new User();
+        user.setUsername(username);
+        user.setEmail(email);
+        user.setRole(role);
+        user.setPassword(password);
+        user.setBalance(balance);
+        return user;
+    }
+}
