8cc2d278cdf65699697382007c4c7a8c1718ba93
RobertBenyamin <robertbenyamino@gmail.com>
2024-04-24 02:31:54 +0700

    Add no-args constructor to ProductReviewDTO

--- src/main/java/id/ac/ui/cs/advprog/gametime/transaction/dto/ProductReviewDTO.java
+++ src/main/java/id/ac/ui/cs/advprog/gametime/transaction/dto/ProductReviewDTO.java
@@ -2,11 +2,13 @@ package id.ac.ui.cs.advprog.gametime.transaction.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class ProductReviewDTO {
     private String authorId;
     private String productId;
