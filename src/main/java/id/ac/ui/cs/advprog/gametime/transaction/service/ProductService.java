37545a17201dd182f4918e6b31ef1d3a13b190c3
Kevin I <kevin.ignatius@ui.ac.id>
2024-04-23 21:11:38 +0700

    testing searching function

---
+++ src/main/java/id/ac/ui/cs/advprog/gametime/transaction/service/ProductService.java
@@ -0,0 +1,21 @@
+import id.ac.ui.cs.advprog.gametime.transaction.model.Product;
+import id.ac.ui.cs.advprog.gametime.transaction.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<Product> findByName(String name) {
+        return productRepository.findByName(name);
+    }
+}
