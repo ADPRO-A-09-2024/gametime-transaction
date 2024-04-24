0312fadc0334bbb1c3566caf55892bf9183e1eb3
RobertBenyamin <robertbenyamino@gmail.com>
2024-04-24 21:13:18 +0700

    Refactor TransactionApplicationTests class

--- src/test/java/id/ac/ui/cs/advprog/gametime/transaction/TransactionApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/gametime/transaction/TransactionApplicationTests.java
@@ -1,9 +1,7 @@
 package id.ac.ui.cs.advprog.gametime.transaction;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class TransactionApplicationTests {
 
 	@Test
