08958a4a8fb622a5eaf3e57e7e4a4a5980b1277f
RobertBenyamin <robertbenyamino@gmail.com>
2024-05-08 17:30:20 +0700

    fix: remove insecure configuration

--- src/main/java/id/ac/ui/cs/advprog/gametime/transaction/exceptions/GlobalExceptionHandler.java
+++ src/main/java/id/ac/ui/cs/advprog/gametime/transaction/exceptions/GlobalExceptionHandler.java
@@ -1,22 +1,19 @@
 package id.ac.ui.cs.advprog.gametime.transaction.exceptions;
 
 import io.jsonwebtoken.ExpiredJwtException;
-import io.jsonwebtoken.security.SignatureException;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ProblemDetail;
-import org.springframework.security.access.AccessDeniedException;
-import org.springframework.security.authentication.AccountStatusException;
-import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.http.HttpStatusCode;
+import io.jsonwebtoken.security.SignatureException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.security.authentication.AccountStatusException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     public ProblemDetail handleSecurityException(Exception exception) {
         ProblemDetail errorDetail = null;
-
-        exception.printStackTrace();
 
         if (exception instanceof AccountStatusException) {
             errorDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(403), exception.getMessage());
