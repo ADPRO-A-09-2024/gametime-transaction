d6cdf13aad37c4dcb40de958e149194663c2d048
RobertBenyamin <robertbenyamino@gmail.com>
2024-05-08 11:51:29 +0700

    [REFACTOR]  add validations method and fix the affected method

--- src/main/java/id/ac/ui/cs/advprog/gametime/transaction/service/ProductReviewServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/gametime/transaction/service/ProductReviewServiceImpl.java
@@ -6,41 +6,43 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.concurrent.CompletableFuture;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.beans.factory.annotation.Autowired;
 
-import id.ac.ui.cs.advprog.gametime.transaction.dto.ProductReviewDTO;
+import id.ac.ui.cs.advprog.gametime.transaction.model.User;
 import id.ac.ui.cs.advprog.gametime.transaction.model.Product;
 import id.ac.ui.cs.advprog.gametime.transaction.model.ProductReview;
-import id.ac.ui.cs.advprog.gametime.transaction.model.User;
+import id.ac.ui.cs.advprog.gametime.transaction.dto.ProductReviewDTO;
+import id.ac.ui.cs.advprog.gametime.transaction.repository.UserRepository;
 import id.ac.ui.cs.advprog.gametime.transaction.repository.ProductRepository;
 import id.ac.ui.cs.advprog.gametime.transaction.repository.ProductReviewRepository;
-import id.ac.ui.cs.advprog.gametime.transaction.repository.UserRepository;
 
 @Service
 public class ProductReviewServiceImpl implements ProductReviewService {
     @Autowired
-    private ProductReviewRepository productReviewRepository;
+    public ProductReviewRepository productReviewRepository;
 
     @Autowired
-    private UserRepository userRepository;
+    public UserRepository userRepository;
 
     @Autowired
-    private ProductRepository productRepository;
+    public ProductRepository productRepository;
 
     @Override
     public ProductReview addProductReview(ProductReviewDTO productReviewDTO) {
-        User author = userRepository.findById(Integer.parseInt(productReviewDTO.getAuthorId()))
+        User author = userRepository.findById(validateAuthorID(productReviewDTO.getAuthorId()))
                 .orElseThrow(() -> new IllegalArgumentException("Author not found"));
-        Product product = productRepository.findById(UUID.fromString(productReviewDTO.getProductId()))
+        Product product = productRepository.findById(validateProductID(productReviewDTO.getProductId()))
                 .orElseThrow(() -> new IllegalArgumentException("Product not found"));
+        String content = validateContent(productReviewDTO.getContent());
+        double rating = validateRating(productReviewDTO.getRating());
 
         ProductReview productReview = ProductReview.builder()
                 .author(author)
                 .product(product)
-                .content(productReviewDTO.getContent())
-                .rating(Double.parseDouble(productReviewDTO.getRating()))
+                .content(content)
+                .rating(rating)
                 .build();
         productReview = productReviewRepository.save(productReview);
 
@@ -56,15 +58,15 @@ public class ProductReviewServiceImpl implements ProductReviewService {
     }
 
     @Override
-    public List<ProductReview> getProductReviewsByProduct(UUID productId) {
+    public List<ProductReview> getProductReviewsByProduct(String productId) {
         return productReviewRepository.findAll().stream()
-                .filter(productReview -> productReview.getProduct().getId().equals(productId)).toList();
+                .filter(productReview -> productReview.getProduct().getId().equals(validateProductID(productId))).toList();
     }
 
     @Override
-    public List<ProductReview> getProductReviewsByAuthor(Integer authorId) {
+    public List<ProductReview> getProductReviewsByAuthor(String authorId) {
         return productReviewRepository.findAll().stream()
-                .filter(productReview -> productReview.getAuthor().getId().equals(authorId)).toList();
+                .filter(productReview -> productReview.getAuthor().getId().equals(validateAuthorID(authorId))).toList();
     }
 
     @Override
@@ -73,23 +75,19 @@ public class ProductReviewServiceImpl implements ProductReviewService {
     }
 
     @Override
-    public ProductReview updateProductReview(UUID productReviewId, ProductReviewDTO productReviewDTO) {
-        ProductReview productReview = productReviewRepository.findById(productReviewId)
+    public ProductReview updateProductReview(String productReviewId, ProductReviewDTO productReviewDTO) {
+        ProductReview productReview = productReviewRepository.findById(validateProductReviewID(productReviewId))
                 .orElseThrow(() -> new IllegalArgumentException("Review not found"));
-        User author = userRepository.findById(Integer.parseInt(productReviewDTO.getAuthorId()))
+        User author = userRepository.findById(validateAuthorID(productReviewDTO.getAuthorId()))
                 .orElseThrow(() -> new IllegalArgumentException("Author not found"));
-        Product product = productRepository.findById(UUID.fromString(productReviewDTO.getProductId()))
+        Product product = productRepository.findById(validateProductID(productReviewDTO.getProductId()))
                 .orElseThrow(() -> new IllegalArgumentException("Product not found"));
-
-        double rating = Double.parseDouble(productReviewDTO.getRating());
-
-        if (rating < 0 || rating > 5) {
-            throw new IllegalArgumentException("Rating must be between 0 and 5");
-        }
+        String content = validateContent(productReviewDTO.getContent());
+        double rating = validateRating(productReviewDTO.getRating());
 
         productReview.setAuthor(author);
         productReview.setProduct(product);
-        productReview.setContent(productReviewDTO.getContent());
+        productReview.setContent(content);
         productReview.setRating(rating);
         productReview = productReviewRepository.save(productReview);
 
@@ -100,8 +98,8 @@ public class ProductReviewServiceImpl implements ProductReviewService {
     }
 
     @Override
-    public void deleteProductReview(UUID productReviewId) {
-        ProductReview productReview = productReviewRepository.findById(productReviewId)
+    public void deleteProductReview(String productReviewId) {
+        ProductReview productReview = productReviewRepository.findById(validateProductReviewID(productReviewId))
                 .orElseThrow(() -> new IllegalArgumentException("Review not found"));
         productReviewRepository.delete(productReview);
 
@@ -119,7 +117,7 @@ public class ProductReviewServiceImpl implements ProductReviewService {
         Product product = productRepository.findById(productId)
                 .orElseThrow(() -> new IllegalArgumentException("Product not found"));
 
-        List<ProductReview> productReviews = getProductReviewsByProduct(productId);
+        List<ProductReview> productReviews = getProductReviewsByProduct(productId.toString());
 
         double totalRating = productReviews.stream()
                 .mapToDouble(ProductReview::getRating)
@@ -135,5 +133,53 @@ public class ProductReviewServiceImpl implements ProductReviewService {
         }
 
         productRepository.save(product);
+    }
+
+    @Override
+    public UUID validateProductReviewID(String productReviewId) {
+        UUID productReviewIdValue;
+        try {
+            productReviewIdValue = UUID.fromString(productReviewId);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Product Review ID must be a valid UUID");
+        }
+        return productReviewIdValue;
+    }
+
+    @Override
+    public int validateAuthorID(String authorId) {
+        int authorIdValue;
+        try {
+            authorIdValue = Integer.parseInt(authorId);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Author ID must be an integer");
+        }
+        return authorIdValue;
+    }
+
+    @Override
+    public UUID validateProductID(String productId) {
+        UUID productIdValue;
+        try {
+            productIdValue = UUID.fromString(productId);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Product ID must be a valid UUID");
+        }
+        return productIdValue;
+    }
+
+    @Override
+    public String validateContent(String content) {
+        if (content == null || content.isEmpty())
+            throw new IllegalArgumentException("Content cannot be empty");
+        return content;
+    }
+
+    @Override
+    public double validateRating(String rating) {
+        double ratingValue = Double.parseDouble(rating);
+        if (ratingValue < 0 || ratingValue > 5)
+            throw new IllegalArgumentException("Rating must be between 0 and 5");
+        return ratingValue;
     }
 }
