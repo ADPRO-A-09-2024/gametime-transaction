43b181132bd0ef2fc8d8e14ce075f72edfd382b6
“rachelzn” <racheltjengerari@gmail.com>
2024-05-10 01:40:47 +0700

    fix: finish CartBuilder implementation

--- src/main/java/id/ac/ui/cs/advprog/gametime/transaction/model/Builder/CartBuilder.java
+++ src/main/java/id/ac/ui/cs/advprog/gametime/transaction/model/Builder/CartBuilder.java
@@ -1,37 +1,42 @@
 package id.ac.ui.cs.advprog.gametime.transaction.model.Builder;
 
 import id.ac.ui.cs.advprog.gametime.transaction.model.Cart;
-import id.ac.ui.cs.advprog.gametime.transaction.model.Product;
+import id.ac.ui.cs.advprog.gametime.transaction.model.CartItem;
 import id.ac.ui.cs.advprog.gametime.transaction.model.User;
+import id.ac.ui.cs.advprog.gametime.transaction.model.Enum.UserRole;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 public class CartBuilder {
     private UUID id;
-    private User buyer;
-    private List<Product> productList = new ArrayList<>();
+    private User user;
+    private List<CartItem> items = new ArrayList<>();
 
-    public CartBuilder setId(UUID id) {
+    public CartBuilder id(UUID id) {
         this.id = id;
         return this;
     }
 
-    public CartBuilder setBuyer(User buyer) {
-        this.buyer = buyer;
+    public CartBuilder user(User user) {
+        if (!UserRole.BUYER.equals(user.getRole())) {
+            throw new IllegalArgumentException("Only users with the 'BUYER' role can have a cart.");
+        }
+        this.user = user;
         return this;
     }
 
-    public CartBuilder setProductList(List<Product> productList) {
-        this.productList = productList;
+    public CartBuilder items(List<CartItem> items) {
+        this.items = new ArrayList<>(items); // Defensively copies the list
         return this;
     }
 
     public Cart build() {
         Cart cart = new Cart();
-        cart.setId(this.id);
-        cart.setBuyer(this.buyer);
-        cart.setProductList(this.productList);
+        cart.setId(id);
+        cart.setUser(user);
+        cart.setItems(items);
         return cart;
     }
 }
